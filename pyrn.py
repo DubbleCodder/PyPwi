3e218058935bedc172df331bf945ab6da86cc011
python coder <147849151+DubbleCodder@users.noreply.github.com>
2024-06-29 20:00:47 -0400

    Add files via upload

---
+++ pyrn.py
@@ -0,0 +1,16 @@
+from time import *
+
+class file:
+    def __init__(self, file, amt):
+        self.fn = open(file, "r")
+        self.amt = amt
+        Lines = self.fn.readlines()
+
+        count = 0
+
+        for line in Lines:
+            sleep(self.amt)
+            count += 1
+            print("{}".format(line.strip()))
+
+file("thing.txt", 0.09)
